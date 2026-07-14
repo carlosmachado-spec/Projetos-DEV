6ce95927d00d9998c77eb005e03e0f72f01071e6
Carlos Machado <calberto15@gmail.com>
2026-07-13 22:59:39 -0300

    Funções para criar notes e função salvar as notas no local storage.

--- Projetos/DevNotes/JavaScript/script.js
+++ Projetos/DevNotes/JavaScript/script.js
@@ -4,20 +4,42 @@ const noteInput = document.querySelector("#noteContent")
 const addNoteBtn = document.querySelector(".addNote")
 /* Funções */
 
+/* Variavel para criar um ID partir do 1 */
+ let idCounter = 1;
+
+
+/* Função do botão para salvar os dados e gerar um note com a função createNote */
+function showNotes() {
+    getNotes().forEach((note)=>{
+        const noteElement = createNote(note.id, note.content, note.fixed)
+
+        notesContainer.appendChild(noteElement)
+    })
+}
+
 function addNote(){
+    const notes = getNotes()
+
+
     const noteObject = {
         id: idCounter++,
         content: noteInput.value,
         fixed: false,
-    }
+    }    
 
     const noteElement = createNote(noteObject.id, noteObject.content)
 
     notesContainer.appendChild(noteElement)
- } 
-/* Variavel para criar um ID partir do 1 */
- let idCounter = 1;
 
+    noteInput.value = ""
+
+    notes.push(noteObject);
+
+    saveNotes(notes)
+ }     
+
+
+ /* Função parar criar e montar uma note dentro do html */
  function createNote (id, content, fixed){
     const element = document.createElement("div")
     element.classList.add("note")
@@ -29,11 +51,27 @@ function addNote(){
     return element
 
  }
- 
+
+  /* Local Storage */
+ function getNotes(){
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]")
+
+    return notes
+ }
+
+
+ function saveNotes(notes){
+    localStorage.setItem("notes", JSON.stringify(notes))
+ }
+
 /* Eventos */
 
 addNoteBtn.addEventListener("click", () => addNote())
         
 
+
+/* Inicialização */
+
+showNotes();
 
 
