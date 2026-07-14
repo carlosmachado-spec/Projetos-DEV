4287fdcf4ccec975749958c44229d640cd1a62a1
Carlos Machado <calberto15@gmail.com>
2026-07-13 22:30:31 -0300

    java script para criar note dentro do html

--- Projetos/DevNotes/JavaScript/script.js
+++ Projetos/DevNotes/JavaScript/script.js
@@ -1,1 +1,39 @@
-console.log("hello world");
+/* Elementos */
+const notesContainer = document.querySelector("#notesContainer")
+const noteInput = document.querySelector("#noteContent")
+const addNoteBtn = document.querySelector(".addNote")
+/* Funções */
+
+function addNote(){
+    const noteObject = {
+        id: idCounter++,
+        content: noteInput.value,
+        fixed: false,
+    }
+
+    const noteElement = createNote(noteObject.id, noteObject.content)
+
+    notesContainer.appendChild(noteElement)
+ } 
+/* Variavel para criar um ID partir do 1 */
+ let idCounter = 1;
+
+ function createNote (id, content, fixed){
+    const element = document.createElement("div")
+    element.classList.add("note")
+    const textarea = document.createElement("textarea")
+    textarea.value = content
+    textarea.placholder = "Adicione algum texto"
+    element.appendChild(textarea)
+
+    return element
+
+ }
+ 
+/* Eventos */
+
+addNoteBtn.addEventListener("click", () => addNote())
+        
+
+
+
